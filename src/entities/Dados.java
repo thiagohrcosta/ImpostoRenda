d29b0d801dd2694525d589e978b2debf592f17f1
thiagohrcosta <thiago_henrique86@hotmail.com>
2019-12-27 17:27:58 -0300

    Add files via upload
    Criação do projeto base.

---
+++ src/entities/Dados.java
@@ -0,0 +1,31 @@
+package entities;
+
+public abstract class Dados {
+	
+	private String nome;
+	private double rendaAnual;
+	
+	public Dados(String nome, double rendaAnual) {
+		this.nome = nome;
+		this.rendaAnual = rendaAnual;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getRendaAnual() {
+		return rendaAnual;
+	}
+
+	public void setRendaAnual(double rendaAnual) {
+		this.rendaAnual = rendaAnual;
+	}
+	
+		
+
+}
---
+++ src/entities/PessoaFisica.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class PessoaFisica extends Dados {
+	
+	private double gastoComSaude;
+	
+	public PessoaFisica(String nome, double rendaAnual) {
+		super(nome, rendaAnual);
+		
+	}
+
+	
+
+}
